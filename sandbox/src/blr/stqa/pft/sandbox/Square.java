edd317921113670b4ccf6467c7d477d67ff0330b
Anton <tonych375@gmail.com>
2019-06-25 13:25:43 +0300

    Added new Classes, Objects and Constructors. Methods where moved out.

---
+++ sandbox/src/blr/stqa/pft/sandbox/Square.java
@@ -0,0 +1,14 @@
+package blr.stqa.pft.sandbox;
+
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+   this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
